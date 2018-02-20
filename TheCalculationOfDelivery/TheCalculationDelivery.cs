0155ebe44ccc80bbfc010da65011a1f5bfe1b7b8
Степан Митрофанов <stepan.mitrofanov.2015@mail.ru>
2018-02-20 17:18:15 +0400

    fix

---
+++ TheCalculationOfDelivery/TheCalculationDelivery.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace TheCalculationOfDelivery
+{
+    class TheCalculationDelivery
+    {
+        public IWebDriver driver { get; set; }
+        public string text { get; set; }
+        TimeSpan timeout = new TimeSpan(00, 00, 15);
+
+        public TheCalculationDelivery(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Action()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://bolshoy.itech-test.ru/payment-and-delivery/");
+
+            Actions actions = new Actions(driver);
+
+            var country = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.ClassName("label")));
+            actions.MoveToElement(country[1]).Click().Build().Perform();
+
+            var scroll= (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.ClassName("selectric-scroll")));
+            actions.MoveToElement(scroll[1]).SendKeys("Росс" + Keys.Enter).Build().Perform();
+
+            var city = (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.Name("DHL[CITY_TO]")));
+            city.SendKeys("Ульяновск");
+            var index=(new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.Name("DHL[ZIP_CODE_TO]")));
+            index.SendKeys("432000");
+            var weight= (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.Name("DHL[WEIGHT]")));
+            weight.SendKeys("100");
+            weight.Submit();
+            text= (new WebDriverWait(driver, timeout)).Until(ExpectedConditions.ElementIsVisible(By.ClassName("no-mar"))).Text;
+        }
+    }
+}
--- TheCalculationOfDelivery/UnitTest1.cs
+++ TheCalculationOfDelivery/UnitTest1.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
 namespace TheCalculationOfDelivery
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        IWebDriver driver;
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
         {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Delivery()
+        {
+            string actual = "1003 рублей";
+            TheCalculationDelivery del = new TheCalculationDelivery(driver);
+            del.Action();
+            Assert.AreEqual(del.text, actual);
         }
     }
 }
